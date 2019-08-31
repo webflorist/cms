e9b7f6d3f3dde3101982c508cb4d6520a86cf083
Gerald Buttinger <gerald@code.florist>
2019-09-01 01:05:38 +0200

    re-add CmsLinkPayload

---
+++ src/Components/Payload/CmsLinkPayload.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Webflorist\Cms\Components\Payload;
+
+use Webflorist\HtmlFactory\Payload\Abstracts\Payload;
+use Webflorist\IconFactory\Payload\IconPayload;
+
+/**
+ * Payload for a Link.
+ *
+ * Class CmsLinkPayload
+ * @package Webflorist\Cms
+ *
+ * Default properties:
+ * ===================
+ * @property string                      $href               href of Link
+ *
+ * Setters for default properties:
+ * ===============================
+ * @method   $this                      href(string $href)
+ *
+ */
+class CmsLinkPayload extends Payload
+{
+}
